24bf06ab2f3cfb8bc53a6151c374fca99fbbfe69
borchicaD <borka.phoenix@gmail.com>
2020-12-27 13:52:57 +0100

    nothing much has changed

---
+++ TodoWithEF/Controllers/TodoItemsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TodoWithEF.Models;
+
+namespace TodoWithEF.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        private TODOEntities db = new TODOEntities();
+
+        // GET: TodoItems
+        public ActionResult Index()
+        {
+            return View(db.TodoItems.ToList());
+        }
+
+        // GET: TodoItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TodoItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Item")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TodoItems.Add(todoItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Item")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todoItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TodoItem todoItem = db.TodoItems.Find(id);
+            db.TodoItems.Remove(todoItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
